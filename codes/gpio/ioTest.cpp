ef81e7c6d5124d3a85d64dcea2332f304d89d374
gfast2 <gfast2@hotmail.com>
2017-05-15 16:19:18 +0200

    Add todos for going on testing with single GPIO pin on Beagleobne & add file .project (suspectely generatet by Eclipse.

--- codes/gpio/ioTest.cpp
+++ codes/gpio/ioTest.cpp
@@ -1,3 +1,8 @@
+/*
+ * Tester toggle one GPIO on Beaglebone
+ *
+ */
+
 #include <iostream>
 #include <string>
 #include <unistd.h> 
@@ -11,6 +16,10 @@ int main(int argc, char *argv[]){
 	cout << "Testing the GPIO Pin P8_11." << endl;
 
 	gpio_export(LEDGPIO); // It's exported automatically in Linux Kernel 4.4.36, (debian 8.7)
+
+	// TODO: Setup the very 7 different GIPOs that belonges to Display control
+	//       If I'd need some extra efforts to workaround these perhapse "zugeordnet" pins
+
 	gpio_set_dir(LEDGPIO, OUTPUT_PIN);
 	
 	// Flash the LED pin 10 times
