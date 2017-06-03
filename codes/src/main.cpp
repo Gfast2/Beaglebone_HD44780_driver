39b948be89d61a0803c6fd1a63e237e5ca0e3cb3
gfast2 <gfast2@hotmail.com>
2017-06-03 09:21:51 +0200

    Add "std=c++11" as compiler arguments & The display works again. But not stable (because of hardware issue I believe).

--- codes/src/main.cpp
+++ codes/src/main.cpp
@@ -14,7 +14,17 @@
 #include <inttypes.h>
 #include "./gpio/SimpleGPIO.h"
 #include <typeinfo>
+
+// For sleep better:
+// https://stackoverflow.com/questions/158585/how-do-you-add-a-timed-delay-to-a-c-program
+#include <chrono> // For sleep type
+#include <thread> // For sleep type
+
 using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
+
+// TODO: usleep() 's arguments are microseconds!!!
 
 // gpio number on Beaglebone
 // pin name:			 RS  RW  EN  D4  D5  D6  D7
@@ -34,23 +44,14 @@ void initPinMode() {
 // 		 Put this pin high before I change datas on DB pins
 // 		 Put this pin low that is acutally what "send()" means
 void send() {
-	usleep(1); // the minimum time is 450 nano second, here I do 5000 nano sec.
 	gpio_set_value(GPIO[2], HIGH);
-	usleep(5); // the minimum time is 450 nano second, here I do 5000 nano sec.
+	usleep(500); // the minimum time is 450 nano second, here I do 5000 nano sec.
+//	sleep_for( nanoseconds(1000) );
 	gpio_set_value(GPIO[2], LOW);
 }
 
 void setData(uint8_t data) { // Argument is a 4bit instruction set
-
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
-	const int w = CHECK_BIT(data, 0);
-	const int a = CHECK_BIT(data, 1);
-	const int b = CHECK_BIT(data, 2);
-	const int c = CHECK_BIT(data, 3);
-	printf("bits: 1 2 3 4\n");
-	printf("      %u %u %u %u\n", w, a, b, c );
-
-	// I literally spend 90% time of the night on figuring out the priority issue here...
 	gpio_set_value(GPIO[3], ((data & 0b0001) != 0 ? HIGH : LOW));
 	gpio_set_value(GPIO[4], ((data & 0b0010) != 0 ? HIGH : LOW));
 	gpio_set_value(GPIO[5], ((data & 0b0100) != 0 ? HIGH : LOW));
@@ -77,7 +78,7 @@ void setRS_LOW(){
 
 // get busy flag state in 4bit mode
 bool getBF(){
-	usleep(1);
+	usleep(1000);
 	setModeRead();
 	gpio_set_value(GPIO[0], LOW);
 	gpio_set_dir(GPIO[6], INPUT_PIN);
@@ -85,24 +86,39 @@ bool getBF(){
 	// Do this checking in while loop till BF is get LOW -> 0 again
 	while(bf != 0){
 		gpio_set_value(GPIO[2], HIGH);
-		usleep(5); // the minimum time is 450 nano second, here I do 5000 nano sec.
+		usleep(500); // the minimum time is 450 nano second, here I do 5000 nano sec.
+//		sleep_for( nanoseconds(1000) );
 		gpio_get_value(GPIO[6], &bf);
 		gpio_set_value(GPIO[2], LOW);
-		usleep(1);
+		usleep(100);
 		gpio_set_value(GPIO[2], HIGH);
-		usleep(5); // the minimum time is 450 nano second, here I do 5000 nano sec.
+		usleep(500); // the minimum time is 450 nano second, here I do 5000 nano sec.
+//		sleep_for( nanoseconds(1000) );
 		gpio_set_value(GPIO[2], LOW);
-		//	usleep(1);
+		usleep(100);
 		cout << "++++ BF: " << bf << " ++++"<< endl;
 	}
 	gpio_set_dir(GPIO[6], OUTPUT_PIN);
 	return true;
 }
 
+// Human understandable position (0 -> characterPerLine x Line - 1)
+int positionTrans(int p){
+	const int h=16, l=2; // TODO: These settings should be placed somewhere else
+	if(p < h){
+		return p - 1;
+	} else if(p > h*l){
+		return 41 + h - 1;
+	}
+	return 41 + p - 16 - 1;
+}
+
 // Set the position of the cursor, "Set CGRAM addresss"
 void moveCursor(unsigned int p){
 	setRS_LOW();
 	setModeWrite();
+	int hdPosition = positionTrans(5);
+	//	int finalPosi = 0b10000000 | hdPosition;
 
 	// Hard coded position: -> 5 ( start from 0)
 	setData(0b1000); send();
@@ -110,15 +126,13 @@ void moveCursor(unsigned int p){
 
 	if (cin.get() == '\n') cout << "." << endl;
 
+	// The first character on the second line -> 41
 	setData(0b1010); send();
-	setData(0b1001); send(); // The second character on the second line
+	setData(0b1001); send();
 
-//	if (cin.get() == '\n') cout << "." << endl;
-//
-//	setData(0b1101); send();
-//	setData(0b0001); send(); // The second character on the second line
+	if (cin.get() == '\n') cout << "." << endl;
 
-	getBF(); // TODO: I believe I have some interesting problem: For each new command, I check the BF but not after each executed function
+	getBF();
 }
 
 void displayChar(char c){
@@ -155,39 +169,50 @@ void clear(){
 	cout << "FINISH clear()" << endl;
 }
 
-int main(int argc, char *argv[]) {
+void sendData(int data) {
+	setData( (data|0xF0)>>4 ); send();
+	usleep(5);
+	setData(  data|0x0F     ); send();
+	usleep(5);
+}
 
+int main(int argc, char *argv[]) {
 	cout << "=== INIT PIN STATE ===" << endl;
 	initPinMode();
 
 	cout << "=== INIT STEP ONE: TURN TO 4-BIT MODE ===" << endl;
 	setModeWrite();
-	setData(0b0010); send();
-	setData(0b1000); send();
+//	setData(0b0010); send();
+//	setData(0b1000); send();
+	sendData(0b00101000);
 	getBF();
 
 	cout << "==== INIT STEP TWO: FUNCTION SET ====" << endl;
 	setModeWrite();
-	setData(0b0010); send();
-	setData(0b1000); send();
+//	setData(0b0010); send();
+//	setData(0b1000); send();
+	sendData(0b00101000);
 	getBF();
 
 	cout << "==== INIT STEP THREE: DISPLAY ON/OFF ====" << endl;
 	setModeWrite();
-	setData(0b0000); send();
-	setData(0b1111); send();
+//	setData(0b0000); send();
+//	setData(0b1111); send();
+	sendData(0b00001111);
 	getBF();
 
 	cout << "==== INIT STEP FOUR: CLEAR DISPLAY ====" << endl;
 	setModeWrite();
-	setData(0b0000); send();
-	setData(0b0001); send();
+//	setData(0b0000); send();
+//	setData(0b0001); send();
+	sendData(0b00000001);
 	getBF();
 
 	cout << "==== INIT STEP FIVE: ENTRY> MODE SET ====" << endl;
 	setModeWrite();
-	setData(0b0000); send();
-	setData(0b0110); send();
+//	setData(0b0000); send();
+//	setData(0b0110); send();
+	sendData(0b00000110);
 	getBF();
 
 	cout << "And now all init steps are finished, press Enter to finish the init process" << endl;
@@ -198,7 +223,6 @@ int main(int argc, char *argv[]) {
 	print("123456789 123456789 ");
 	getBF();
 
-	// Clear display
 	clear();
 
 	moveCursor(5);
@@ -207,5 +231,3 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
-//	if (cin.get() == '\n') cout << "." << endl; // This will hack the program till the user press enter key to going on
